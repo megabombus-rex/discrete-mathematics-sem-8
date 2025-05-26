1085f211366fbef45fc1908f4f961170c5a4db9e
megabombus-rex <czechumennakompie@gmail.com>
2025-05-26 10:19:49 +0200

    Added empty algorithms

--- Lab03/src/algorithms.py
+++ Lab03/src/algorithms.py
@@ -1,17 +1,18 @@
 import networkx as nx
 from itertools import product
 
-class MaxCut:
+class MaxCutSolver:
     def __call__(self, graph:nx.Graph):
-        raise NotImplementedError("MaxCut is an abstract class. Use child class.")
+        raise NotImplementedError("MaxCutSolver is an abstract class. Use child class.")
     
-class BruteForceMaxCut(MaxCut):
+class BruteForceMaxCutSolver(MaxCutSolver):
     def __call__(self, graph:nx.Graph):
         nodes = list(graph.nodes())
         n = len(nodes)
         max_cut_value = 0
         best_partition = None
         
+        # cut x edges then x + 1 etc. (n times) and select the best value, 2^n
         for assignment in product([0, 1], repeat = n):
             cut_value = 0
             group = dict(zip(nodes, assignment))
@@ -25,3 +26,15 @@ class BruteForceMaxCut(MaxCut):
                 best_partition = group.copy()
 
         return max_cut_value, best_partition
+    
+class GuirobiMaxCutSolver(MaxCutSolver):
+    def __call__(self, graph):
+        raise NotImplementedError("GuirobiMaxCutSolver not implemented yet.")
+    
+class GoemansWilliamsonMaxCutSolver(MaxCutSolver):
+    def __call__(self, graph):
+        raise NotImplementedError("GoemansWilliamsonMaxCutSolver not implemented yet.")
+    
+class QAOAMaxCutSolver(MaxCutSolver):
+    def __call__(self, graph):
+        raise NotImplementedError("QAOAMaxCutSolver not implemented yet.")
