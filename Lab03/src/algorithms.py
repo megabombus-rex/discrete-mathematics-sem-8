a760ede8ce47e6a7fed1825190f149e89959b721
megabombus-rex <czechumennakompie@gmail.com>
2025-05-26 12:39:02 +0200

    Added gurobi and goemans williamson solvers

--- Lab03/src/algorithms.py
+++ Lab03/src/algorithms.py
@@ -1,5 +1,10 @@
 import networkx as nx
+import numpy as np
+import cvxpy as cp
 from itertools import product
+import gurobipy as gp
+from gurobipy import GRB
+from constants import GOEMANS_WILLIAMSON_RETRIES_COUNT
 
 class MaxCutSolver:
     def __call__(self, graph:nx.Graph):
@@ -28,12 +33,88 @@ class BruteForceMaxCutSolver(MaxCutSolver):
         return max_cut_value, best_partition
     
 class GuirobiMaxCutSolver(MaxCutSolver):
-    def __call__(self, graph):
-        raise NotImplementedError("GuirobiMaxCutSolver not implemented yet.")
+    def __call__(self, graph:nx.Graph):
+        model = gp.Model("MaxCut")
+
+        # Binary variables for vertex partitioning
+        x = model.addVars(graph.nodes, vtype=GRB.BINARY, name="x")
+
+        # Auxiliary variables for linearization
+        y = model.addVars(graph.edges, vtype=GRB.BINARY, name="y")
+
+        # Objective function: maximize total weight of cut edges
+        model.setObjective(
+            gp.quicksum((x[i] + x[j] - 2 * y[i, j]) for i, j in graph.edges),
+            GRB.MAXIMIZE
+        )
+
+        # Add McCormick linearization constraints
+        for i, j in graph.edges:
+            model.addConstr(y[i, j] <= x[i])
+            model.addConstr(y[i, j] <= x[j])
+            model.addConstr(y[i, j] >= x[i] + x[j] - 1)
+            model.addConstr(y[i, j] >= 0)
+
+        model.optimize()
+
+        if model.Status == GRB.OPTIMAL:
+            cut_value = model.ObjVal
+            partition = {v: int(x[v].X) for v in graph.nodes}
+            return cut_value, partition
+        else:
+            return None, None
     
 class GoemansWilliamsonMaxCutSolver(MaxCutSolver):
-    def __call__(self, graph):
-        raise NotImplementedError("GoemansWilliamsonMaxCutSolver not implemented yet.")
+    def __call__(self, graph:nx.Graph):
+        n = graph.number_of_nodes()
+        nodes = list(graph.nodes())
+        index = {node: i for i, node in enumerate(nodes)}
+        
+        W = np.zeros((n, n))
+        for u, v, data in graph.edges(data=True):
+            weight = data.get("weight", 1)
+            i, j = index[u], index[v]
+            W[i, j] = W[j, i] = weight
+            
+        X = cp.Variable((n, n), PSD=True)
+        
+        # Objective: maximize 0.25 * sum of w_ij * (1 - X_ij)
+        objective = cp.Maximize(0.25 * cp.sum(cp.multiply(W, 1 - X)))
+        
+        # Constraints: diagonal elements of X must be 1
+        constraints = [cp.diag(X) == 1]
+        
+        # Solve the SDP
+        problem = cp.Problem(objective, constraints)
+        problem.solve()
+        
+        # Cholesky-like decomposition of X (rounding step)
+        eigvals, eigvecs = np.linalg.eigh(X.value)
+        eigvals[eigvals < 0] = 0  # Remove numerical noise
+        V = eigvecs @ np.diag(np.sqrt(eigvals))
+
+        best_cut = 0
+        best_partition = None
+
+        # Repeat randomized rounding
+        for _ in range(GOEMANS_WILLIAMSON_RETRIES_COUNT):
+            r = np.random.randn(V.shape[1])
+            r /= np.linalg.norm(r)
+
+            assignment = V @ r >= 0
+            group = {node: int(assignment[index[node]]) for node in nodes}
+
+            # Count cut value
+            cut_value = 0
+            for u, v, data in graph.edges(data=True):
+                if group[u] != group[v]:
+                    cut_value += 1
+
+            if cut_value > best_cut:
+                best_cut = cut_value
+                best_partition = group
+
+        return best_cut, best_partition
     
 class QAOAMaxCutSolver(MaxCutSolver):
     def __call__(self, graph):
