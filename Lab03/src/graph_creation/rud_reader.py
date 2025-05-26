88965fa14a215581da12de926e899ff19de31efd
megabombus-rex <czechumennakompie@gmail.com>
2025-05-26 10:19:15 +0200

    Added test data with reader

---
+++ Lab03/src/graph_creation/rud_reader.py
@@ -0,0 +1,24 @@
+import networkx as nx
+
+class RudyGraphReader:
+    @classmethod
+    def read_rud(self, path):
+        G = nx.Graph()
+        
+        with open(path, "r") as file:
+            first_line = file.readline()
+            
+            node_count = first_line.strip().split(' ')[0]
+            edge_list = []
+                    
+            while first_line:
+                first_line = file.readline()
+                if not first_line:
+                    continue
+                encoded_edge = first_line.strip().split(' ')
+                G.add_edge(int(encoded_edge[0].strip()), int(encoded_edge[1].strip()))
+                print(f'Added edge ({encoded_edge[0]}, {encoded_edge[1]})')
+                
+        return G                
+                
+                
