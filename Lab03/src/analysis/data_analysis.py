0d85d774d9326d0cedc40ce6e648b5a05aa0252d
megabombus-rex <czechumennakompie@gmail.com>
2025-06-04 11:04:30 +0200

    data_analysis added

---
+++ Lab03/src/analysis/data_analysis.py
@@ -0,0 +1,17 @@
+import csv
+
+def read_results_csv(self, path):
+    with open(path, "r") as file:
+            first_line = file.readline()
+            
+            node_count = first_line.strip().split(' ')[0]
+            edge_list = []
+                    
+            while first_line:
+                first_line = file.readline()
+                if not first_line:
+                    continue
+                encoded_edge = first_line.strip().split(' ')
+                G.add_edge(int(encoded_edge[0].strip()), int(encoded_edge[1].strip()))
+                print(f'Added edge ({encoded_edge[0]}, {encoded_edge[1]})')
+    pass
